1aaf3adde46becbaf5659a83c3a9b4907c8d60f1
CharlesHahn <3447180435@qq.com>
2022-01-18 22:14:18 +0800

    modified csv2plot.py

--- csv2plot.py
+++ csv2plot.py
@@ -2,6 +2,29 @@
 ## date : 20220118
 
 import matplotlib.pyplot as plt
+from matplotlib import pylab as pylab
+
+# 绘图控制参数
+myparams = {
+    'axes.labelsize'  : '14',
+    'xtick.labelsize' : '14',
+    'ytick.labelsize' : '14',
+    'ytick.direction' : 'in', 
+    # 'xtick.bottom'    : False,
+    'xtick.bottom'    : True,
+    'xtick.direction' : 'in',
+    'lines.linewidth' : '2',
+    'axes.linewidth'  : '1',
+    'legend.fontsize' : '14',
+    #'legend.loc'     : 'upper right',
+    'legend.fancybox' : False,
+    'legend.frameon'  : False,
+    'font.family'     : 'Arial',
+    # 'figure.dpi'      : 300,
+    'savefig.dpi'     : 300,
+}
+pylab.rcParams.update(myparams)
+
 
 ## read csv file
 with open("rmsd.csv", 'r') as fo:
@@ -19,9 +42,10 @@ time_list = [ t/1000 for t in time_list ]
 
 ## plot time vs rmsd
 
-plt.plot(time_list, rmsd_list)
+plt.plot(time_list, rmsd_list, label="test")
 plt.xlabel("Time (ns)")
 plt.ylabel(yname)
+plt.legend()
 plt.show()
 
 
